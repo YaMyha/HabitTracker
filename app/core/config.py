0f99255b51f73591b781148d99b042e9099de216
Kurochka Ryaba <you@example.com>
2025-10-06 23:49:20 +0300

    set up env for github

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,13 @@
+import os
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = str(Path(__file__).resolve().parent.parent.parent / ".env")
